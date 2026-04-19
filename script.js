3278f513946ad8933b9eee30a6726bc21d90f46a
Sushant-Bilage <01fe24bcs293@kletech.ac.in>
2026-04-19 15:34:01 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,48 @@
 const uploadBox = document.getElementById("uploadBox");
 const fileInput = document.getElementById("fileInput");
 
-// click → open file
+const previewContainer = document.getElementById("previewContainer");
+const previewImage = document.getElementById("previewImage");
+const fileName = document.getElementById("fileName");
+const fileSize = document.getElementById("fileSize");
+const removeBtn = document.getElementById("removeBtn");
+
+// click upload
 uploadBox.addEventListener("click", () => fileInput.click());
 
 // file selected
 fileInput.addEventListener("change", () => {
-  if (fileInput.files.length > 0) {
-    const file = fileInput.files[0];
-    alert("Selected: " + file.name);
+  const file = fileInput.files[0];
+  if (!file) return;
 
-    // next page later
-    // window.location.href = "analyze.html";
-  }
+  const reader = new FileReader();
+
+  reader.onload = () => {
+    previewImage.src = reader.result;
+
+    // ✅ SAVE IMAGE FOR ANALYZE PAGE
+    localStorage.setItem("uploadedImage", reader.result);
+  };
+
+  reader.readAsDataURL(file);
+
+  fileName.textContent = file.name;
+  fileSize.textContent = (file.size / 1024).toFixed(2) + " KB";
+
+  previewContainer.classList.remove("hidden");
+  uploadBox.style.display = "none";
 });
 
-// drag over
-uploadBox.addEventListener("dragover", (e) => {
-  e.preventDefault();
-  uploadBox.style.borderColor = "#0F766E";
+// remove file
+removeBtn.addEventListener("click", () => {
+  previewContainer.classList.add("hidden");
+  uploadBox.style.display = "block";
+  fileInput.value = "";
 });
 
-// drag leave
-uploadBox.addEventListener("dragleave", () => {
-  uploadBox.style.borderColor = "#E2E3E5";
-});
+// ✅ GO TO ANALYZE PAGE
+const analyzeBtn = document.querySelector(".analyze-btn");
 
-// drop file
-uploadBox.addEventListener("drop", (e) => {
-  e.preventDefault();
-  const file = e.dataTransfer.files[0];
-  alert("Dropped: " + file.name);
+analyzeBtn.addEventListener("click", () => {
+  window.location.href = "analyze.html";
 });
